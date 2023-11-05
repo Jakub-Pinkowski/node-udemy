04a75589ee9715fe18d261fde37efcba61f14e2d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-05 13:18:54 +0100

    Finished module 102

--- Section_7/models/product.js
+++ Section_7/models/product.js
@@ -7,26 +7,45 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        const p = path.join(__dirname, '..', 'data', 'products.json')
         fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
+            if (err) {
+                console.error('Error reading file:', err)
+                return
             }
+
+            let products = []
+            try {
+                products = JSON.parse(fileContent)
+            } catch (parseError) {
+                console.error('Error parsing JSON:', parseError)
+            }
+
             products.push(this)
+
             fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
+                if (err) {
+                    console.error('Error writing file:', err)
+                }
             })
         })
     }
 
-    static fetchAll() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    static fetchAll(callback) {
+        const p = path.join(__dirname, '..', 'data', 'products.json')
         fs.readFile(p, (err, fileContent) => {
             if (err) {
-                return []
+                console.error('Error reading file:', err)
+                callback([])
+            } else {
+                try {
+                    const products = JSON.parse(fileContent)
+                    callback(products)
+                } catch (parseError) {
+                    console.error('Error parsing JSON:', parseError)
+                    callback([])
+                }
             }
-            return JSON.parse(fileContent)
         })
     }
 }
