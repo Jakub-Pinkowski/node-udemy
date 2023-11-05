c377f495c0651f8642d83f72663e789398c15cdb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-05 13:23:53 +0100

    Finished module 103

--- Section_7/models/product.js
+++ Section_7/models/product.js
@@ -1,28 +1,33 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(__dirname, '..', 'data', 'products.json')
+
+const getProductsFromFile = (callback) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            console.error('Error reading file:', err)
+            callback([])
+        } else {
+            try {
+                const products = JSON.parse(fileContent)
+                callback(products)
+            } catch (parseError) {
+                console.error('Error parsing JSON:', parseError)
+                callback([])
+            }
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t
     }
 
     save() {
-        const p = path.join(__dirname, '..', 'data', 'products.json')
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                console.error('Error reading file:', err)
-                return
-            }
-
-            let products = []
-            try {
-                products = JSON.parse(fileContent)
-            } catch (parseError) {
-                console.error('Error parsing JSON:', parseError)
-            }
-
+        getProductsFromFile((products) => {
             products.push(this)
-
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 if (err) {
                     console.error('Error writing file:', err)
@@ -32,20 +37,6 @@ module.exports = class Product {
     }
 
     static fetchAll(callback) {
-        const p = path.join(__dirname, '..', 'data', 'products.json')
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                console.error('Error reading file:', err)
-                callback([])
-            } else {
-                try {
-                    const products = JSON.parse(fileContent)
-                    callback(products)
-                } catch (parseError) {
-                    console.error('Error parsing JSON:', parseError)
-                    callback([])
-                }
-            }
-        })
+        getProductsFromFile(callback)
     }
 }
